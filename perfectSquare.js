e41843afb6d2a4e009ede4e3432a141c1e5f6de2
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-26 10:13:38 -0600

    completed

--- perfectSquare.js
+++ perfectSquare.js
@@ -2,7 +2,7 @@ const perfectSquare = num => {
     let squareRoot = Math.sqrt(num)
 
     if (Math.round(squareRoot) === squareRoot) {
-        return Math.pow(++squareRoot, 2)
+        return (squareRoot + 1) * (squareRoot + 1)
     }   return -1
 }
 console.log(perfectSquare(9))
